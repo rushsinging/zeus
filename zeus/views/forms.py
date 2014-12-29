875b33f1c0cdd999cadb3ccd49b7432737b85d1f
Jade <littleyu.hit@gmail.com>
2014-12-29 17:31:15 +0800

    init

---
+++ zeus/views/forms.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+
+from wtforms import Form, validators
+from wtforms import StringField, PasswordField, HiddenField
+
+
+class DisplaySuccessForm(Form):
+    display = HiddenField()
+    success = HiddenField(validators=[
+        validators.URL(), validators.Optional()
+    ])
+
+
+class LoginForm(Form):
+    email = StringField(validators=[validators.Required()])
+    password_hash = PasswordField(validators=[validators.Required()])
+
+
+class SignUpForm(Form):
+    email = StringField(validators=[validators.Required()])
+    password_hash = PasswordField(validators=[validators.Required()])
+    nickname = StringField()
